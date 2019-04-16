4721e8c9be1d2a159e4bd58a18c3bee9ec2f5508
ger4ivan <49093875+ger4ivan@users.noreply.github.com>
2019-04-16 20:22:33 +0300

    Update install_ruby_mongodb.sh

--- install_ruby_mongodb.sh
+++ install_ruby_mongodb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm requirements
-rvm install 2.4.1
-rvm use 2.4.1 --default
-gem install bundler -V --no-ri --no-rdoc
+sudo gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+sudo curl -sSL https://get.rvm.io | bash -s stable
+sudo source ~/.rvm/scripts/rvm
+sudo rvm requirements
+sudo rvm install 2.4.1
+sudo rvm use 2.4.1 --default
+sudo gem install bundler -V --no-ri --no-rdoc
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt-get update
